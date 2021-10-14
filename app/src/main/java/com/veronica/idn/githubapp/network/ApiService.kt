bcc15dd7c636cf7e44fb83200164b90dfe0b91ac
veroanggra <veronicaputri497@gmail.com>
2021-10-14 23:01:11 +0700

    update api service, change function name

--- app/src/main/java/com/veronica/idn/githubapp/network/ApiService.kt
+++ app/src/main/java/com/veronica/idn/githubapp/network/ApiService.kt
@@ -1,8 +1,8 @@
 package com.veronica.idn.githubapp.network
 
 import com.veronica.idn.githubapp.BuildConfig
-import com.veronica.idn.githubapp.model.DetailUser
-import com.veronica.idn.githubapp.model.SearchResult
+import com.veronica.idn.githubapp.data.model.DetailUser
+import com.veronica.idn.githubapp.data.model.SearchResult
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
@@ -11,7 +11,7 @@ import retrofit2.http.Query
 interface ApiService {
     @GET("search/users?q=followers%3A>%3D1000&ref=searchresults&s=followers&type=Users")
     @Headers("Authorization: token ${BuildConfig.KEY}")
-    suspend fun getPopularUser(): SearchResult
+    suspend fun getUser(): SearchResult
 
     @GET("search/users")
     @Headers("Authorization: token ${BuildConfig.KEY}")
