6edc1bd2f49dbaceafadad1b17163fd491881583
veroanggra <veronicaputri497@gmail.com>
2021-10-14 21:36:15 +0700

    create api service and result class

---
+++ app/src/main/java/com/veronica/idn/githubapp/network/ApiService.kt
@@ -0,0 +1,24 @@
+package com.veronica.idn.githubapp.network
+
+import com.veronica.idn.githubapp.BuildConfig
+import com.veronica.idn.githubapp.model.DetailUser
+import com.veronica.idn.githubapp.model.SearchResult
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("search/users?q=followers%3A>%3D1000&ref=searchresults&s=followers&type=Users")
+    @Headers("Authorization: token ${BuildConfig.KEY}")
+    suspend fun getPopularUser(): SearchResult
+
+    @GET("search/users")
+    @Headers("Authorization: token ${BuildConfig.KEY}")
+    suspend fun getSearchUser(@Query("q") username: String): SearchResult
+
+    @GET("users/{username}")
+    @Headers("Authorization: token ${BuildConfig.KEY}")
+    suspend fun getDetailUser(@Path("username") username: String): DetailUser
+
+}
